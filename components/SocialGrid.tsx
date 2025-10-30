f0433ecaf3c99af1042360ddfec5ada1296900b1
Pedro Márcio Di Pietro <pedromarcioap@gmail.com>
2025-10-30 20:21:41 -0300

    feat: Remove static social icons and background script
    The social icons are now dynamically generated in the frontend, eliminating the need for hardcoded SVG definitions in the `SocialGrid` component.
    
    The `background.js` service worker, which was responsible for managing context menus and platform sharing logic, has been removed as this functionality is now handled within the frontend application. This simplifies the extension's architecture and reduces its footprint.

--- components/SocialGrid.tsx
+++ components/SocialGrid.tsx
@@ -1,37 +1,104 @@
-
 import React from 'react';
-import SocialButton from './SocialButton';
 import type { SocialPlatform, ShareParams } from '../types';
+import SocialButton from './SocialButton';
 
-// Using inline SVGs for icons to avoid extra dependencies.
-const TwitterIcon = () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full text-sky-400"><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.39.106-.803.163-1.227.163-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></svg>;
-const FacebookIcon = () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full text-blue-600"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"></path></svg>;
-const LinkedinIcon = () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full text-blue-500"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667h-3.554v-12h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.284zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019h-3.564v-12h3.564v12zM22.225 0h-20.45c-.984 0-1.774.79-1.774 1.774v20.451c0 .984.79 1.774 1.774 1.774h20.45c.984 0 1.775-.79 1.775-1.774v-20.451c0-.984-.791-1.774-1.775-1.774z"></path></svg>;
-const RedditIcon = () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full text-orange-500"><path d="M12.021 0C5.383 0 0 5.383 0 12.021s5.383 12.021 12.021 12.021 12.021-5.383 12.021-12.021S18.659 0 12.021 0zM12.021 21.365c-5.163 0-9.344-4.181-9.344-9.344s4.181-9.344 9.344-9.344 9.344 4.181 9.344 9.344-4.181 9.344-9.344 9.344zm-3.328-11.459c-.496 0-.899.403-.899.899s.403.899.899.899.899-.403.899-.899-.403-.899-.899-.899zm6.656 0c-.496 0-.899.403-.899.899s.403.899.899.899.899-.403.899-.899-.403-.899-.899-.899zm-1.832 3.129c-1.353 1.353-3.547 1.353-4.9 0l-.1-.1c-.131-.131-.131-.343 0-.474s.343-.131.474 0l.1.1c1.159 1.159 3.047 1.159 4.206 0 .131-.131.343-.131.474 0s.131.343 0 .474z"></path></svg>;
-const WhatsappIcon = () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full text-green-500"><path d="M12.031 6.172c-3.181 0-5.767 2.586-5.767 5.767s2.586 5.767 5.767 5.767c3.18 0 5.767-2.586 5.767-5.767s-2.587-5.767-5.767-5.767zm0 10.3c-2.5 0-4.533-2.033-4.533-4.533s2.033-4.533 4.533-4.533c2.5 0 4.533 2.033 4.533 4.533s-2.033 4.533-4.533 4.533zm6.176-14.072c-1.171-1.171-2.736-1.812-4.409-1.812-1.672 0-3.238.641-4.409 1.812-1.171 1.171-1.812 2.736-1.812 4.409 0 1.672.641 3.238 1.812 4.409l-1.357 4.969 5.068-1.332c1.153.714 2.486 1.104 3.874 1.104 3.428 0 6.221-2.793 6.221-6.221 0-1.673-.641-3.238-1.812-4.409zm-1.767 7.018c-.283.504-.986.9-1.4.956-.379.052-1.018.009-1.504-.176-.567-.215-1.116-.549-1.589-.955-1.29-1.104-2.123-2.6-2.21-2.748-.086-.148-.686-1.023-.686-1.882 0-.859.458-1.282.686-1.458.228-.176.489-.215.631-.215.176 0 .343.009.485.009.176 0 .388-.061.597.436.241.597.832 2.019.9 2.167.069.148.118.315 0 .485-.118.176-.207.283-.352.427-.145.148-.292.315-.41.436-.118.127-.25.26-.118.494.132.232.588.973 1.258 1.579.881.823 1.626 1.096 1.845 1.185.219.088.447.07.622-.052.207-.148.455-.494.574-.661.118-.167.241-.148.396-.088s1.018.476 1.185.565c.167.088.283.132.327.207.044.074.044.427-.239.931z"></path></svg>;
-const TelegramIcon = () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full text-sky-500"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7.253 8.216l-3.253 11.784c-.205.748-.724 1-1.285.63l-4.102-3.23-1.99 1.912c-.218.218-.475.436-.927.436l.32-4.195 7.904-7.464c.345-.316-.067-.487-.522-.172l-9.815 6.188-4.04-1.26c-.732-.232-.748-.84.143-1.248l16.488-6.398c.613-.238 1.16.198.966.906z"></path></svg>;
-const EmailIcon = () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full text-gray-400"><path d="M12 12.713l-11.985-9.213h23.97l-11.985 9.213zm0 2.574l-12-9.287v15h24v-15l-12 9.287z"></path></svg>;
+// SVG Icons for social platforms
+const TwitterIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" /></svg>
+);
+
+const FacebookIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" /></svg>
+);
+
+const LinkedInIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+);
+
+const RedditIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.833 13.583c0 .833-.667 1.5-1.5 1.5s-1.5-.667-1.5-1.5c0-1.667-1.333-3-3-3s-3 1.333-3 3c0 .833-.667 1.5-1.5 1.5s-1.5-.667-1.5-1.5c0-3.167 2.583-5.75 5.75-5.75s5.75 2.583 5.75 5.75zm-3.25-3.167c-.833 0-1.5-.667-1.5-1.5s.667-1.5 1.5-1.5 1.5.667 1.5 1.5-.667 1.5-1.5 1.5zm-5.167 0c-.833 0-1.5-.667-1.5-1.5s.667-1.5 1.5-1.5 1.5.667 1.5 1.5-.667 1.5-1.5 1.5zm1.583 6c1.5 1.167 3.5 1.167 5 0 .167.333.25.75.25 1.167 0 1.25-1.917 2.25-4.25 2.25s-4.25-1-4.25-2.25c0-.417.083-.833.25-1.167z" /></svg>
+);
+
+const WhatsAppIcon = () => (
+    <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12.04 2c-5.46 0-9.91 4.45-9.91 9.91 0 1.75.46 3.38 1.25 4.81L2 22l5.42-1.39c1.37.74 2.92 1.18 4.62 1.18h.01c5.46 0 9.91-4.45 9.91-9.91s-4.45-9.91-9.91-9.91zM17.5 14.3c-.28-.14-1.67-.82-1.93-.91s-.45-.14-.64.14-.73.91-.89 1.1s-.33.19-.61.07c-.28-.12-1.18-.44-2.24-1.38s-1.7-1.59-1.9-1.86c-.2-.26-.05-.4.12-.54s.28-.33.42-.5c.14-.16.19-.28.28-.47s.05-.35-.02-.49c-.08-.14-.64-1.54-.87-2.1s-.46-.48-.64-.49h-.52c-.18 0-.47.07-.71.35s-.91.89-.91 2.17.93 2.52 1.06 2.7c.13.18 1.81 2.76 4.38 3.84.6.26 1.08.41 1.45.53.59.19 1.13.16 1.56.1.48-.07 1.67-.68 1.9-1.33s.24-1.22.16-1.34c-.06-.12-.2-.18-.47-.32z"/></svg>
+);
+
+const TelegramIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.69 6.54l-1.44 6.78c-.14.65-.54.81-.97.5l-2.22-1.63-1.08 1.04c-.12.12-.22.22-.44.22l.16-2.28 4.25-3.85c.18-.16-.04-.25-.31-.09l-5.26 3.3-2.18-.68c-.65-.2-1.02-.61-.69-1.2l1.62-5.02c.2-.62.77-1.02 1.46-1.02h.01c.22 0 .43.04.62.13z" /></svg>
+);
+
+const EmailIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z" /></svg>
+);
+
+const PinterestIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.477 2 2 6.477 2 12c0 4.237 2.865 7.828 6.743 9.14.03-.23.04-.51.04-.79 0-.91-.35-2.16-.35-2.16s-.26-.52-.26-1.29c0-1.21.7-2.12 1.58-2.12.74 0 1.1.55 1.1 1.21 0 .74-.47 1.84-.72 2.87-.2.88.44 1.6 1.32 1.6 1.58 0 2.79-1.67 2.79-4.11 0-2.2-1.55-3.75-3.8-3.75-2.58 0-4.22 1.94-4.22 4.02 0 .74.28 1.55.63 2.05.07.1.08.18.06.28l-.18.7c-.05.23-.2.28-.4.18-1.45-.65-2.35-2.61-2.35-4.5 0-3.32 2.4-6.2 6.86-6.2 3.63 0 6.35 2.58 6.35 5.8 0 3.6-2.22 6.4-5.3 6.4-1.07 0-2.08-.55-2.43-1.18l-.53 2.02c-.18.7-.7 1.62-1.03 2.18.91.29 1.86.44 2.85.44 5.523 0 10-4.477 10-10S17.523 2 12 2z" /></svg>
+);
+
 
 export const platforms: SocialPlatform[] = [
-  { name: 'Twitter', icon: <TwitterIcon />, shareUrl: ({ url, text, title }) => `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${encodeURIComponent(text || title)}` },
-  { name: 'Facebook', icon: <FacebookIcon />, shareUrl: ({ url, text }) => `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}&quote=${encodeURIComponent(text)}` },
-  { name: 'LinkedIn', icon: <LinkedinIcon />, shareUrl: ({ url, title, text }) => `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}&summary=${encodeURIComponent(text)}` },
-  { name: 'Reddit', icon: <RedditIcon />, shareUrl: ({ url, title }) => `https://www.reddit.com/submit?url=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}` },
-  { name: 'WhatsApp', icon: <WhatsappIcon />, shareUrl: ({ url, text }) => `https://api.whatsapp.com/send?text=${encodeURIComponent(text + ' ' + url)}` },
-  { name: 'Telegram', icon: <TelegramIcon />, shareUrl: ({ url, text }) => `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(text)}` },
-  { name: 'Email', icon: <EmailIcon />, shareUrl: ({ url, title, text }) => `mailto:?subject=${encodeURIComponent(title)}&body=${encodeURIComponent(text + '\n\n' + url)}` },
+  {
+    name: 'X (Twitter)',
+    icon: <TwitterIcon />,
+    shareUrl: ({ url, text }: ShareParams) => `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${encodeURIComponent(text)}`,
+  },
+  {
+    name: 'Facebook',
+    icon: <FacebookIcon />,
+    shareUrl: ({ url, text }: ShareParams) => `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}&quote=${encodeURIComponent(text)}`,
+  },
+  {
+    name: 'LinkedIn',
+    icon: <LinkedInIcon />,
+    shareUrl: ({ url, title, text }: ShareParams) => `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}&summary=${encodeURIComponent(text)}`,
+  },
+  {
+    name: 'Reddit',
+    icon: <RedditIcon />,
+    shareUrl: ({ url, title }: ShareParams) => `https://www.reddit.com/submit?url=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}`,
+  },
+  {
+    name: 'WhatsApp',
+    icon: <WhatsAppIcon />,
+    shareUrl: ({ url, text }: ShareParams) => `https://api.whatsapp.com/send?text=${encodeURIComponent(text ? `${text} - ${url}` : url)}`,
+  },
+  {
+    name: 'Telegram',
+    icon: <TelegramIcon />,
+    shareUrl: ({ url, text }: ShareParams) => `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(text)}`,
+  },
+  {
+    name: 'E-mail',
+    icon: <EmailIcon />,
+    shareUrl: ({ url, title, text }: ShareParams) => `mailto:?subject=${encodeURIComponent(title)}&body=${encodeURIComponent(text ? `${text}\n\n${url}` : url)}`,
+  },
+  {
+    name: 'Pinterest',
+    icon: <PinterestIcon />,
+    shareUrl: ({ url, text }: ShareParams) => `https://www.pinterest.com/pin/create/button/?url=${encodeURIComponent(url)}&description=${encodeURIComponent(text)}`,
+  },
 ];
 
-interface SocialGridProps extends ShareParams {}
+interface SocialGridProps {
+  url: string;
+  title: string;
+  text: string;
+}
 
 const SocialGrid: React.FC<SocialGridProps> = ({ url, title, text }) => {
   const shareParams: ShareParams = { url, title, text };
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
       {platforms.map((platform) => (
-        <SocialButton key={platform.name} platform={platform} shareParams={shareParams} />
+        <SocialButton
+          key={platform.name}
+          platform={platform}
+          shareParams={shareParams}
+        />
       ))}
     </div>
   );
 };
+
 export default SocialGrid;
